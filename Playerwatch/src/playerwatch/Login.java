de571ecb4fab4740ecc31909d602ca38cf123e3f
OhRidgeInAll <hollandg@sheridancollege.ca>
2017-04-17 00:15:41 -0400

    Add files via upload
    The beginning of the biggest method in the world

--- Playerwatch/src/playerwatch/Login.java
+++ Playerwatch/src/playerwatch/Login.java
@@ -18,6 +18,30 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import playerwatch.heroes.DVa;
+import playerwatch.heroes.Soldier76;
+import playerwatch.heroes.Ana;
+import playerwatch.heroes.Bastion;
+import playerwatch.heroes.Genji;
+import playerwatch.heroes.Roadhog;
+import playerwatch.heroes.Hero;
+import playerwatch.heroes.Hanzo;
+import playerwatch.heroes.JunkRat;
+import playerwatch.heroes.Lucio;
+import playerwatch.heroes.McCree;
+import playerwatch.heroes.Mei;
+import playerwatch.heroes.Mercy;
+import playerwatch.heroes.Pharah;
+import playerwatch.heroes.Reaper;
+import playerwatch.heroes.Reinhardt;
+import playerwatch.heroes.Sombra;
+import playerwatch.heroes.Symmetra;
+import playerwatch.heroes.Torbjorn;
+import playerwatch.heroes.Tracer;
+import playerwatch.heroes.Widowmaker;
+import playerwatch.heroes.Winston;
+import playerwatch.heroes.Zarya;
+import playerwatch.heroes.Zenyatta;
 
 /**
  *
@@ -31,8 +55,10 @@ and then passes the JSONObject to the main class.
 This class was handled by Garrett
 */
 public class Login {
+    String battletag;
 
     public JSONObject login(String battletag) throws IOException, ParseException {
+        this.battletag = battletag;
         InputStream response;
         
         File file = new File(battletag + "-heroes.txt");
@@ -84,6 +110,48 @@ public class Login {
         return null;
     }
     
+    public void initializeStats(Ana ana, Bastion bastion, DVa dva, Genji genji, Hanzo hanzo, Hero hero, JunkRat junkrat, Lucio lucio, McCree mccree, Mei mei, Mercy mercy, Pharah pharah, Reaper reaper, Reinhardt reinhardt, Roadhog roadhog, Soldier76 soldier76, Sombra sombra, Symmetra symettra, Torbjorn torbjorn, Tracer tracer, Widowmaker widowmaker, Winston winston, Zarya zarya, Zenyatta zenyatta){
+        FileReader reader = null;
+        try {
+            File file = new File(battletag + "-heroes.txt");
+            File genFile = new File(battletag + ".txt");
+            JSONParser parser = new JSONParser();
+            reader = new FileReader(battletag + "-heroes.txt");
+            JSONObject root = (JSONObject)parser.parse(reader);
+            JSONObject us = (JSONObject)root.get("us");
+            JSONObject usheroes = (JSONObject)us.get("heroes");
+            JSONObject usherostats = (JSONObject)usheroes.get("stats");
+            JSONObject usHeroQuickplay = (JSONObject)usherostats.get("quickplay");
+            JSONObject usRoadhog = (JSONObject)usHeroQuickplay.get("roadhog");
+            JSONObject usRoadhogStats = (JSONObject)usRoadhog.get("hero_stats");
+            JSONObject usRoadhogAvgStats = (JSONObject)usRoadhog.get("average_stats");
+            //Setting Roadhog Values
+            roadhog.setEnemiesHooked((double)usRoadhogStats.get("enemies_hooked"));
+            roadhog.setEnemiesHookedAverage((double)usRoadhogAvgStats.get("enemies_hooked_average"));
+            roadhog.setEnemiesHookedMostGame((double)usRoadhogStats.get("enemies_hooked"));
+            roadhog.setHookAccuracy((double)usRoadhogStats.get("hook_accuracy"));
+            roadhog.setHookAccuracyBestGame((double)usRoadhogStats.get("hook_accuracy_best_in_game"));
+            roadhog.setHookAttempts((double)usRoadhogStats.get("hooks_attempted"));
+            roadhog.setWholeHogKills((double)usRoadhogStats.get("enemies_hooked"));
+            roadhog.setWholeHogKillsAverage((double)usRoadhogStats.get("enemies_hooked"));
+            roadhog.setWholeHogKillsMostGame((double)usRoadhogStats.get("enemies_hooked"));
+            roadhog.setSelfHealing((double)usRoadhogStats.get("enemies_hooked"));
+            roadhog.setSelfHealingAverage((double)usRoadhogStats.get("enemies_hooked"));
+            roadhog.setSelfHealingMostGame((double)usRoadhogStats.get("enemies_hooked"));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParseException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
     //public String
 
 }
